ee36965bb74def1d273796a4d119d1f2483829eb
heine.shimizu <heine.shimizu@gmail.com>
2020-10-02 00:50:31 +0900

    content 動作確認、セミコロン有りに変更

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    h1
+      |{{ doc.title }}
+    div
+      |{{ doc.description }}
+    nuxt-content(
+      :document="doc"
+      )
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const doc = await $content(`posts/${params.slug}`).fetch();
+    return { doc };
+  },
+};
+</script>
+
+<style lang="scss"></style>
